ff5daf5c51832e79932e51135583f874ba91d529
VoreXxx <vorex.00@mail.ru>
2023-10-30 21:00:56 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static String[] splitString (String str) {
+        int lenght = str.length();
+        int middleIndex = lenght / 2;
+        String firstHalf = str.substring(0, middleIndex);
+        String secondHaf = str.substring(middleIndex);
+        return new String[]{firstHalf, secondHaf};
+    }
+    public static void main(String[] args) {
+        String str = "Промокашка";
+        String[] halves = splitString(str);
+        System.out.println("Первая часть " + halves[0]);
+        System.out.println("Второая часть " + halves[1]);
+    }
+}
+
